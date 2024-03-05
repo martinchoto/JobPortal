f3e9ed216d7cdb2361be4c3b1b2d49bfc6a34923
martinchoto <martin_stalev03@abv.bg>
2024-03-06 00:27:16 +0200

    created project

---
+++ Job-Portal/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Job_Portal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
