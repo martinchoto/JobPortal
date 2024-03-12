b60d992b31f8e7c029f507626eb50bb9716e9c56
martinchoto <martin_stalev03@abv.bg>
2024-03-12 19:37:39 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -36,9 +36,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
 app.Run();
